7f50ce75e80f0cbb00dc351471bf7455bb844a2f
yukikoshima <0gv3cj@gmail.com>
2023-01-30 09:35:23 +0900

    動作はするがGEThttp://localhost:24678/_nuxt/のエラーがでる

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
-  css: [
-    'vuetify/lib/styles/main.sass',
-    '@mdi/font/css/materialdesignicons.css',
+  modules: [
+    // https://next.vuetifyjs.com/en/features/treeshaking/
+    // {styles: { configFile: 'assets/scss/settings.scss' }}
+    async (_, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', config => {
+        config?.plugins?.push(
+          vuetify({ styles: { configFile: 'assets/scss/settings.scss' } })
+        )
+      })
+    },
+    '@nuxt/image-edge',
+    '@nuxtjs/google-fonts',
   ],
+  css: ['vuetify/styles', '@mdi/font/css/materialdesignicons.css'],
+  // css: [
+  //   'vuetify/lib/styles/main.sass',
+  //   '@mdi/font/css/materialdesignicons.css',
+  // ],
   build: {
     transpile: ['vuetify'],
   },
-  modules: ['@nuxt/image-edge'],
+  vite: { ssr: { noExternal: ['vuetify'] } },
+  googleFonts: {
+    families: {
+      Yomogi: [400],
+    },
+    download: false,
+    useStylesheet: false,
+  },
 })
